27b94e90322f2f5773e037873be00b1c73998cb3
Amelie Barbosa <amelieelibh@gmail.com>
2018-02-16 13:51:37 -0600

    canvas

--- web/main.dart
+++ web/main.dart
@@ -191,7 +191,7 @@ main() {
   //var game = new Game(width, height, WEBGL, '');
 
 
-  Game game = new Game(800, 600, AUTO, 'phaser-example');
+  Game game = new Game(800, 600, CANVAS, 'phaser-example');
 /*
   dom.SelectElement select = dom.document.getElementById("examples") as dom.SelectElement;
   for (String key in examples.keys) {
